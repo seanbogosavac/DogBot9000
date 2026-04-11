672a8f61518726ffaf42c6793fc514202a748795
Sean Bogosavac <bogosavac.sean@proton.me>
2026-04-11 19:58:17 +0200

    Added secret dog feature

--- bot/loops.py
+++ bot/loops.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import random
 import discord
@@ -5,8 +6,9 @@ from discord.ext import tasks
 from .bot_instance import bot
 
 
-@tasks.loop(hours=24)
+@tasks.loop(time=datetime.time(hour=9, minute=0))
 async def daily_pistouche():
+    # Retrieving channel and raising error if unavailable
     channel = bot.get_channel(os.getenv("DISCORD_CHANNEL"))
     if channel is None:
         try:
@@ -15,18 +17,27 @@ async def daily_pistouche():
             print(f"Failed to fetch channel: {e}")
             return
     
-    folder_path = os.path.join(os.path.dirname(__file__), "../pic")
+    # Choosing between regular dog or secret dog
+    if random.randint(1, 50) == 1:
+        message = "LEGENDARY PULL! YOU GOT A SECRET DOG"
+        folder_path = folder_path = os.path.join(os.path.dirname(__file__), "../pic/secret")
+    else:
+        message = ""
+        folder_path = folder_path = os.path.join(os.path.dirname(__file__), "../pic")
+
+    # [Both cases] Picking random images in the chosen folder
     images = [f for f in os.listdir(folder_path)]
-    
     if not images:
         print("No images found in ../pic")
         return
-    
-    # Pick random image
     image_path = os.path.join(folder_path, random.choice(images))
     
+    # Sending actual message
     try:
-        await channel.send(file=discord.File(image_path))
+        await channel.send(
+            content=message,
+            file=discord.File(image_path)
+        )
         print(f"Sent image: {image_path}")
     except Exception as e:
         print(f"Failed to send image: {e}")
